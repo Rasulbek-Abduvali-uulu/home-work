6dcab05b228610ddde42b1d8baf5b4ef088ce39d
Rasul-back <kyrgyzrasul@gmail.com>
2024-02-14 19:24:53 +0600

    first hw

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+let name = 'John';
+var age = 25;
+const gender = 'male';
+let obj = {
+    name : 'John',
+    age : 34,
+    job : 'teacher'
+}
+let arr = [1,2,3,4,5];
+
+console.log(name);
+console.log(age);
+console.log(gender)
+console.log(obj);
+console.log(arr);
